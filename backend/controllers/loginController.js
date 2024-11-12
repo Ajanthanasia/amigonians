1856e25965cbcaae935b4af8e71d9b676d836f41
ajanthan-hippop <ajanthan.dev@hotelshippo.lk>
2024-11-12 12:08:46 +0530

    ok

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -6,6 +6,7 @@ exports.authLogin = async (req, res) => {
   try {
     let username = req.body.username;
     let password = req.body.password;
+
     if (
       username === null ||
       username === "" ||
@@ -13,18 +14,29 @@ exports.authLogin = async (req, res) => {
       password === ""
     ) {
       res.json(msgs.successRespoonse(false, "failed", [], 422));
-    } else {
-      let user = await User.findOne({ where: { email: username } });
-      if (user === null) {
-        res.json({ status: false, message: "User not founds" });
-      }
-      let passwordValid = await bcrypt.compare(password, user.password);
-      if (passwordValid === false) {
-        res.status(400).json({ status: false, message: "Invalid Password" });
-      } else {
-        res.status(200).json({ status: true, message: "Logined", data: user });
-      }
+      return;
     }
+    
+    
+    
+
+      let user = await User.findOne({ where: { email: username } });
+
+      if (!user ) {
+        res.json({ status: false, message: "User not founds" });
+        return;
+      }
+
+
+      const passwordValid = await bcrypt.compare(password, user.password);
+
+      if (!passwordValid) {
+        res.status(400).json({ status: false, message: "Invalid Password" });
+        return;
+      } 
+
+        res.status(200).json({ status: true, message: "Logined", data: user });
+    
   } catch (err) {
     console.log(err);
   }
